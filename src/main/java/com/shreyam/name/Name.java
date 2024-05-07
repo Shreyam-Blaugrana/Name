32f07aa34d01aea58337ca59397d8e76fc1ca25c
Shreyam-Blaugrana <shreeyamsahu383@gmail.com>
2024-05-07 11:43:50 +0530

    Initial commit

---
+++ src/main/java/com/shreyam/name/Name.java
@@ -0,0 +1,18 @@
+package com.shreyam.name;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Name {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Name.class, args);
+        printName("Your Name");
+    }
+
+    private static void printName(String name) {
+        System.out.println("Hello, " + name + "Shreyam");
+    }
+}
+
